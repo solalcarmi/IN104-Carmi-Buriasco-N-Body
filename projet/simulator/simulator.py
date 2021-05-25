56f54a39ac2d82646340243d757d6e03b8a20140
solalcarmi <81634142+solalcarmi@users.noreply.github.com>
2021-05-25 12:30:02 +0200

    Update simulator.py

--- projet/simulator/simulator.py
+++ projet/simulator/simulator.py
@@ -1,3 +1,4 @@
+
 class Simulator:
     def __init__(self, world, Engine, Solver):
         self.t = 0
@@ -17,8 +18,37 @@ class Simulator:
 
         self.solver = Solver(self.engine.derivatives, self.t, y0)
 
+    def check_for_collisions(self):
+        y = self.solver.y0
+        to_delete = []
+        for i in range(len(self.world)):
+            for j in range(len(self.world)):
+                b_i = self.world.get(i)
+                b_j = self.world.get(j)
+                if i != j and b_i.does_collide(b_j):
+                    strongest_body = b_i
+                    weakest_body = b_j
+                    if b_i.mass <= b_j.mass:
+                        to_delete.append(i)
+                        strongest_body, weakest_body = b_j, b_i
+                    else:
+                        to_delete.append(j)
+                    strongest_body.mass += weakest_body.mass
+        to_delete.sort(reverse=True)
+        N = len(self.world._bodies)
+        i=0
+        for deletable_body_index in to_delete:
+            y.remove(y[deletable_body_index + 2 * N + 1])
+            y.remove(y[deletable_body_index + 2 * N])
+            y.remove(y[deletable_body_index + 1])
+            y.remove(y[deletable_body_index])
+            i+=1
+
     def step(self, h):
+        import time
+        profiler = time.time()
         y = self.solver.integrate(self.t + h)
+        print("Time took to integrate", time.time() - profiler)
 
         for i in range(len(self.world)):
             b_i = self.world.get(i)
@@ -28,5 +58,5 @@ class Simulator:
 
             b_i.velocity.set_x(y[len(self.world) + 2 * i])
             b_i.velocity.set_y(y[len(self.world) + 2 * i + 1])
-
+            i+=1
         self.t += h
